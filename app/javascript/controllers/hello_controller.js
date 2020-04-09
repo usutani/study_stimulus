811ce89c53da0e43c53fe4652ec0e97b6399160c
Yasuhiro Usutani <y_usutani@mac.com>
2020-04-09 13:29:56 +0900

    Confirm data-hello-amount

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,6 @@
-// Visit The Stimulus Handbook for more details 
+// Visit The Stimulus Handbook for more details
 // https://stimulusjs.org/handbook/introduction
-// 
+//
 // This example controller works with specially annotated HTML like:
 //
 // <div data-controller="hello">
@@ -13,6 +13,20 @@ export default class extends Controller {
   static targets = [ "output" ]
 
   connect() {
+    console.log(`connected, amount: ${this.amount}`)
     this.outputTarget.textContent = 'Hello, Stimulus!'
+  }
+
+  disconnect() {
+    this.amount = this.amount + 1
+    console.log(`disconnected, amount: ${this.amount}`)
+  }
+
+  get amount() {
+    return parseInt(this.data.get("amount"))
+  }
+
+  set amount(value) {
+    this.data.set("amount", value)
   }
 }
